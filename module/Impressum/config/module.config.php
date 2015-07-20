cf7a9305fe04ceca1ebbc7939d80cab96d5349d1
snowiow <marcel.patzwahl@gmail.com>
2015-07-20 18:08:05 +0200

    resolve #14

---
+++ module/Impressum/config/module.config.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'controllers'  => [
+        'invokables' => [
+            'Impressum\Controller\Impressum' => 'Impressum\Controller\ImpressumController',
+        ],
+    ],
+    'router'       => [
+        'routes' => [
+            'members' => [
+                'type'    => 'segment',
+                'options' => [
+                    'route'       => '/impressum[/:action]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                    ],
+                    'defaults'    => [
+                        'controller' => 'Impressum\Controller\Impressum',
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'impressum' => __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/Impressum/src/Impressum/Controller/ImpressumController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Impressum\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class ImpressumController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+    }
+}
